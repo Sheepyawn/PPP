036766d82cc248693863d0c1368860edfecaf3dd
SheepYawn <2276341011@qq.com>
2026-04-21 20:48:25 +0800

    11章环境配置

--- 11/main.cpp
+++ 11/main.cpp
@@ -1,24 +1,34 @@
 #pragma once
 #include "PPP/Simple_window.h"
 #include "PPP/Graph.h"
+#include "PPP.h"
 
 using namespace Graph_lib;
+
 
 int main()
 //  draw two lines
 {
-    Application app;
+    try
+    {
+        Application app;
 
-    constexpr Point x{ 100,100 };
+        constexpr Point x{ 100,100 };
 
-    Simple_window win{ x,600,400,"two lines" };
+        Simple_window win{ x,600,400,"two lines" };
 
-    //Line horizontal{ x,Point{200,100} };                              // make a horizontal line
-    //Line vertical{ Point{150,50},Point{150,150} };              // make a vertical line
+        Line horizontal{ x,Point{200,100} };                              // make a horizontal line
+        Line vertical{ Point{150,50},Point{150,150} };              // make a vertical line
 
-    //win.attach(horizontal);                                                  // attach the lines to the window
-    //win.attach(vertical);
+        win.attach(horizontal);                                                  // attach the lines to the window
+        win.attach(vertical);
 
-    win.wait_for_button();                                                   // display!
+        win.wait_for_button();                                                   // display!
+    }
+    catch (...)
+    {
+        cout << "something went wrong\n";
+    }
 }
+
 
